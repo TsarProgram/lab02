b27a4d3eb2cbc2177f4e96cc527d744e34a14a5f
TsarProgram <octorozhnozlaya@gmail.com>
2021-03-07 17:31:34 +0300

    znaki

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 int main()
 {
-std::string str;//переменные
-getline(std::cin, str);//ввод имени
-std::cout << "Hello World from " << str;//вывод строки
+std::string str;//переменные.
+getline(std::cin, str);//ввод имени.
+std::cout << "Hello World from " << str;//вывод строки.
 }
