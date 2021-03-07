abc5b27f48a15e2c0fac17a6c460d9ded50db687
TsarProgram <octorozhnozlaya@gmail.com>
2021-03-07 17:17:48 +0300

    new style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
-int main()
+int
+main()
 {
 std::string str;//переменные.
 getline(std::cin, str);//ввод имени.
 std::cout << "Hello World from " << str;//вывод строки.
+  std::string str;                         //переменные
+  getline(std::cin, str);                  //ввод имени
+  std::cout << "Hello World from " << str; //вывод строки
 }
